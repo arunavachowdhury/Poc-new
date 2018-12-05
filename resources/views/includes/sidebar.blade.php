c23f97be39fc4772b8973f00231614eb1007225c
Suman Sarkar <sumansarkarwd@gmail.com>
2018-12-05 13:50:00 +0530

    Auth implemented

---
+++ resources/views/includes/sidebar.blade.php
@@ -0,0 +1,64 @@
+<div class="sidebar">
+    <div class="sidebar-inner">
+        <div class="sidebar-logo">
+            <div class="peers ai-c fxw-nw">
+                <div class="peer peer-greed"><a class="sidebar-link td-n" href="index.html" class="td-n">
+                        <div class="peers ai-c fxw-nw">
+                            <div class="peer">
+                                <div class="logo"><img src="assets/static/images/logo.png" alt=""></div>
+                            </div>
+                            <div class="peer peer-greed">
+                                <h5 class="lh-1 mB-0 logo-text">Adminator</h5>
+                            </div>
+                        </div>
+                    </a></div>
+                <div class="peer">
+                    <div class="mobile-toggle sidebar-toggle"><a href="#" class="td-n"><i class="ti-arrow-circle-left"></i></a></div>
+                </div>
+            </div>
+        </div>
+        <ul class="sidebar-menu scrollable pos-r">
+            <li class="nav-item mT-30 active">
+                <a class="sidebar-link" href="index.html" default>
+                    <span class="icon-holder"><i class="c-green-500 ti-home"></i></span>
+                    <span class="title">Dashboard</span>
+                </a>
+            </li>
+            <!-- Tests -->
+            <li class="nav-item dropdown">
+                <a class="dropdown-toggle" href="javascript:void(0);">
+                    <span class="icon-holder"><i class="c-blue-500 ti-files"></i></span>
+                    <span class="title">Tests</span> 
+                    <span class="arrow"><i class="ti-angle-right"></i></span>
+                </a>
+                <ul class="dropdown-menu">
+                    <li><a href="google-maps.html">Customer already Registered?</a></li>
+                    <li><a href="vector-maps.html">New Customer</a></li>
+                </ul>
+            </li>
+            <!-- Review -->
+            <li class="nav-item dropdown">
+                <a class="dropdown-toggle" href="javascript:void(0);">
+                    <span class="icon-holder"><i class="c-blue-500 ti-files"></i></span>
+                    <span class="title">Test Review</span> 
+                    <span class="arrow"><i class="ti-angle-right"></i></span>
+                </a>
+                <ul class="dropdown-menu">
+                    <li><a href="google-maps.html">Show Review List</a></li>
+                </ul>
+            </li>
+            <!-- Jobs -->
+            <li class="nav-item dropdown">
+                <a class="dropdown-toggle" href="javascript:void(0);">
+                    <span class="icon-holder"><i class="c-blue-500 ti-files"></i></span>
+                    <span class="title">Jobs</span> 
+                    <span class="arrow"><i class="ti-angle-right"></i></span>
+                </a>
+                <ul class="dropdown-menu">
+                    <li><a href="google-maps.html">Assign a job</a></li>
+                    <li><a href="google-maps.html">See your jobs</a></li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,15 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/dashboard', function () {
+    return view('includes.dashboard');
+})->middleware('auth');
 
 Route::resource('sample', 'SampleController');
 Route::resource('isstandard', 'ISStandardController');
 Route::resource('testitem', 'TestItemController');
 Route::resource('customer', 'CustomerController');
 Route::resource('test', 'TestController');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
